03f1dacb6dccb12cb3760993d613d4713c68e8a9
Adam Polner <polnera@rsd407.org>
2023-03-30 20:07:25 -0700

    Removed directives that apeared twice

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -5,15 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using GameEngine;
-using SFML.Graphics;
 using SFML.System;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
 namespace MyGame
 {
     class Enemy : GameObject
--- MyGame/Hero.cs
+++ MyGame/Hero.cs
@@ -1,5 +1,4 @@
 ﻿using GameEngine;
-using GameEngine;
 using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
